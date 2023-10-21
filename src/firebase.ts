ea9204dc8ae9bfc2b3be3dce6b34158e786be8ec
kimmoonju-102 <kimmun3549@naver.com>
2023-10-21 19:10:34 +0900

    Feat | login 사용자에게 오류 표시

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdUSvz6ieZi7KrZWCSa0Tb0zvs1sV3C-I",
+  authDomain: "react-twitter-b8533.firebaseapp.com",
+  projectId: "react-twitter-b8533",
+  storageBucket: "react-twitter-b8533.appspot.com",
+  messagingSenderId: "1039154822978",
+  appId: "1:1039154822978:web:49212e0a2818b9fbe212db",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,59 @@
-export default function Login() {
-  return <h1>Login!</h1>;
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./../firebase";
+import { Error, Form, Input, Swither, Title, Wrapper } from "../components/auth-components";
+
+export default function CreateAccount() {
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // 어떤 input이 변경되었는지 알 수 있다.
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    if (isLoading || email === "" || password === "") return;
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Title>Login into x</Title>
+      <Form onSubmit={onSubmit}>
+        <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+        <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required />
+        <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+      <Swither>
+        Don't have an account? {""}
+        <Link to="/create-account">Create one &rarr;</Link>
+      </Swither>
+    </Wrapper>
+  );
 }
